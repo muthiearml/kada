ee3ba0a6f5c5f29df5491b01218da01f2d3e2238
muthiearml <muthiearmalia@gmail.com>
2026-03-04 16:04:00 +0700

    deploy

--- midtrans/midtrans.js
+++ midtrans/midtrans.js
@@ -1,45 +1,48 @@
 import midtransClient from "midtrans-client";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const snap = new midtransClient.Snap({
-    isProduction: false,
-    serverKey: process.env.MIDTRANS_SERVER_KEY,
-    clientKey: process.env.MIDTRANS_CLIENT_KEY
+  isProduction: false,
+  serverKey: process.env.MIDTRANS_SERVER_KEY,
+  clientKey: process.env.MIDTRANS_CLIENT_KEY,
 });
 
-const coreApi = new midtransClient.coreApi({
-    isProduction: false,
-    serverKey: process.env.MIDTRANS_SERVER_KEY,
-    clientKey: process.env.MIDTRANS_CLIENT_KEY
+const coreApi = new midtransClient.CoreApi({
+  isProduction: false,
+  serverKey: process.env.MIDTRANS_SERVER_KEY,
+  clientKey: process.env.MIDTRANS_CLIENT_KEY,
 });
 
 // BUAT TRANSAKSI
 export const createTransaction = async (req, res) => {
-    try {
-        const {amount, first_name, email} = req.body;
+  try {
+    const { amount, first_name, email } = req.body;
 
-        const parameter = {
-            transaction_details: {
-                order_id: "ORDER-" + Date.now(),
-                gross_amount: amount
-            },
-            credit_card: {
-                secure: true
-            },
-            customer_details: {
-                first_name,
-                email
-            }
-        };
+    const parameter = {
+      transaction_details: {
+        order_id: "ORDER-" + Date.now(),
+        gross_amount: amount,
+      },
+      credit_card: {
+        secure: true,
+      },
+      customer_details: {
+        first_name,
+        email,
+      },
+    };
 
-        const transaction = await snap.createTransaction(parameter);
+    const transaction = await snap.createTransaction(parameter);
 
-        res.status(200).json({
-            message: "Transaksi berhasil dibuat!",
-            token: transaction.token,
-            redirect_url: transaction.redirect_url
-        });
-    } catch (error) {
-        console.error("Error createTransaction:", error);
-        res.status(500).json({message: "Gagal membuat transaksi :("});
-    }
+    res.status(200).json({
+      message: "Transaksi berhasil dibuat!",
+      token: transaction.token,
+      redirect_url: transaction.redirect_url,
+    });
+  } catch (error) {
+    console.error("Error createTransaction:", error);
+    res.status(500).json({ message: "Gagal membuat transaksi :(" });
+  }
 };
